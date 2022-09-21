bee370984568578ecb952e327bc9259d84e15b65
Joao Victor Moraes Vieira <88463022+jaummm1@users.noreply.github.com>
2022-09-20 21:34:54 -0300

    confirmar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, child, get, DataSnapshot } from "firebase/database";
+import { useState } from "react";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export default App;
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4Yj-b9w-6QGAuiLcrlu8TAI7pgT3HavI",
+  authDomain: "ghost-1cc15.firebaseapp.com",
+  databaseURL: "https://ghost-1cc15-default-rtdb.firebaseio.com",
+  projectId: "ghost-1cc15",
+  storageBucket: "ghost-1cc15.appspot.com",
+  messagingSenderId: "720278757956",
+  appId: "1:720278757956:web:2a88d45bc0c1e66155d211",
+  measurementId: "G-Q3N07886DR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const dbRef = ref(getDatabase())
+
+export const App = () => {
+    get(child(dbRef, `ranking`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    })
+
+    const  [score, setScore] = useState({})
+     return (
+      Object.keys(score).map(nome => {
+        return <div className="container">
+          <b>{nome}</b>: {score[nome]} <br/>
+        </div>
+      }))
+    }
+
